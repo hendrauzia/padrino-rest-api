ce7393c209396209ead4f142004f993089890a89
Hendra Uzia <hendra.uzia@gmail.com>
2015-05-01 11:15:16 +0700

    Add client to list employee passports

--- public/javascripts/models/employee.js
+++ public/javascripts/models/employee.js
@@ -2,5 +2,7 @@ PadrinoRestApi.Employee = DS.Model.extend({
   name: DS.attr('string'),
 
   company: DS.belongsTo('company', { async: true }),
-  position: DS.belongsTo('position', { async: true })
+  position: DS.belongsTo('position', { async: true }),
+
+  passports: DS.hasMany('passport', { async: true })
 });
---
+++ public/javascripts/models/passport.js
@@ -0,0 +1,3 @@
+PadrinoRestApi.Passport = DS.Model.extend({
+  upload_file_url: DS.attr('string')
+});
